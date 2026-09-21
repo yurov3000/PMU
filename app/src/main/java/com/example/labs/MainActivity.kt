df30fdf1964e8335b6980167b4ff456683a135d4
yurov3000 <yurov3000@gmail.com>
2026-09-21 17:11:03 +0700

    Initial commit Hello App

---
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.labs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.labs.ui.theme.LabsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LabsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingText(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreviewText() {
+    LabsTheme {
+        GreetingText("Android")
+    }
+}
